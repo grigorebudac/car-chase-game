1486790854943f2d991a23d54c2ca732a41bbbe5
Grigore Budac <contact@grigorebudac.com>
2022-03-28 10:39:14 +0200

    make police cars follow the player car

---
+++ Assets/Scripts/SpawnPoliceCar.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoliceCar : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float spawnDelay = 3;
+
+    Transform playerTarget;
+    private float nextSpawnTime;
+
+    private void Start()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(ShouldSpawn())
+        {
+            Spawn();
+        }        
+    }
+
+    public void Spawn()
+    {
+        float x = playerTarget.position.x - Random.Range(10, 30);
+        float z = playerTarget.position.z - Random.Range(10, 30);
+
+        Vector3 spawnPosition = new Vector3(x, 0, z);
+        Instantiate(prefab, spawnPosition, Quaternion.identity);
+
+        nextSpawnTime = Time.time + spawnDelay;
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}
