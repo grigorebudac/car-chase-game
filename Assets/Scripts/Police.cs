eff39dfabbdad5ba5b73062f77423b09ac4c8e8d
Andrei Bostan <andy27hush@gmail.com>
2022-04-25 22:28:35 +0200

    cleand up the code

--- Assets/Scripts/Police.cs
+++ Assets/Scripts/Police.cs
@@ -5,39 +5,26 @@ using UnityEngine.AI;
 
 public class Police : MonoBehaviour
 {
-    private GameObject target;
+    private NavMeshAgent pathFinder;
+    Transform target;
 
-    private Rigidbody myBody;
-    [SerializeField]
-    private float speed = 60f, rotatingSpeed = 20f;
-
-    // Start is called before the first frame update
     void Start()
     {
-        myBody = GetComponent<Rigidbody>();
-        target = GameObject.FindGameObjectWithTag("Player");
+        pathFinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
     }
 
-    void Update()
+    IEnumerator UpdatePath()
     {
-        Vector3 pointToTarget = transform.position - target.transform.position;
-        pointToTarget.Normalize();
+        float refreshRate = 0.25f;
 
-        float value = Vector3.Cross(pointToTarget, transform.forward).y;
-
-        myBody.angularVelocity = rotatingSpeed * value * Vector3.up;
-        myBody.velocity = transform.forward * speed;
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathFinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
     }
-
-    // IEnumerator UpdatePath()
-    // {
-    //     float refreshRate = 0.25f;
-
-    //     while (target != null)
-    //     {
-    //         Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-    //         pathFinder.SetDestination(targetPosition);
-    //         yield return new WaitForSeconds(refreshRate);
-    //     }
-    // }
 }
